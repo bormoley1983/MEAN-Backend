8da7080d0fd22bf7cd10613e18afe02fc4f7ed00
Leonid Zhuravel <Leonidz@Cityshob.com>
2025-09-07 09:29:32 +0300

    p4-done + code cleanup

--- app.js
+++ app.js
@@ -1,49 +1,64 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
+
+mongoose
+  .connect("mongodb://localhost:27017/mean_course")
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 const app = express();
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, resp, next) => {
-    resp.setHeader('Access-Control-Allow-Origin', '*');
-    resp.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    resp.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
+  resp.setHeader("Access-Control-Allow-Origin", "*");
+  resp.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  resp.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  );
+  next();
 });
 
-// app.use((req, resp, next) => {
-//     console.log('In the middleware');
-//     next();
-// });
-
-
-app.post('/api/posts', (req, resp, next) => {
-    const post = req.body;//by bodyParser
-    console.log(post);
+app.post("/api/posts", (req, resp, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((savedPost) => {
+    console.log(savedPost);
     resp.status(201).json({
-        message: 'Post added successfully'
+      message: "Post added successfully",
+      postId: savedPost._id,
     });
+  });
 });
 
-app.use('/api/posts', (req, resp, next) => {
-    const posts = [
-        {
-            id: 'abc',
-            title: 'First post',
-            content: 'This is the first post!'
-        },
-        {
-            id: 'def',
-            title: 'Second post',
-            content: 'This is the second post!'
-        }
-    ];
-
+app.get("/api/posts", (req, resp, next) => {
+  Post.find().then((documents) => {
+    console.log(documents);
     resp.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: posts
+      message: "Posts fetched successfully",
+      posts: documents,
     });
+  });
+});
+
+app.delete("/api/posts/:id", (req, resp, next) => {
+  console.log("Post to be deleted: " + req.params.id);
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    resp.status(200).json({ message: "Post deleted!" });
+  });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,45 +1,46 @@
-const app = require('./app.js');
-const debug = require('debug')("node");
-const  http = require('http');
+const app = require("./app.js");
+const debug = require("debug")("node");
+const http = require("http");
 
-const normalizePort = val =>{
-    var port = parseInt(val, 10);
+const normalizePort = (val) => {
+  var port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val; // named pipe
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-}
+  if (isNaN(port)) {
+    return val; // named pipe
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
 
-const onError = error => {if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 };
 
 const onListening = () => {
-    const addr = server.address();
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + port;
-    console.log('Listening on ' + bind);
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  console.log("Listening on " + bind);
 };
-    
+
 const port = normalizePort(process.env.PORT || "3000");
-app.set('port', port);
+app.set("port", port);
 
 const server = http.createServer(app);
 server.on("error", onError);
